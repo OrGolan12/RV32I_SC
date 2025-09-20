f9e7903aabc3a8172f09687904e75bbb86b58665
OrGolan12 <golanorwd@gmail.com>
2025-09-20 18:36:55 +0300

    Add instruction memory module for data retrieval

---
+++ imem.sv
@@ -0,0 +1,9 @@
+module imem(
+    input logic [31:0] a,
+    output logic [31:0] rd
+);
+    logic [31:0] mem [63:0];
+    initial
+       $readmemh("riscvtest.txt",mem);
+    assign rd = mem[a[31:2]]; // word aligned
+ endmodule
